ab2e189687248bb5696b2cb7a3fb7cb73cdf86aa
Benjamin Malin <benjaminamalin@gmail.com>
2017-10-02 16:50:19 -0400

    Refactor Game into a class component

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -7,13 +7,15 @@ import GuessForm from './guessform';
 import GuessCount from './guesscount';
 import GuessList from './guesslist';
 
-export default function Game() {
-  return (
-    <section class="game">
-      <GuessClue text='clue'/> {/*figure out how to set this  */}
-      <GuessForm />
-      <GuessCount count="0" />
-      <GuessList />
-    </section>
-  );
+export default class Game extends React.Component {
+  render() {
+    return (
+      <section class="game">
+        <GuessClue text='clue'/> {/*figure out how to set this  */}
+        <GuessForm />
+        <GuessCount count="0" />
+        <GuessList />
+      </section>
+    );
+  }
 }
